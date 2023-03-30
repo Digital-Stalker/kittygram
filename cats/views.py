a6cd8c55a2060923570199e4befb0aa4a5eef49e
Александр Ячменев <yachmenyov.alex@yandex.ru>
2023-03-30 19:12:19 +0700

    Lesson 7.

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,61 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Cat
 from .serializers import CatSerializer
+
+
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+"""
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class APICatDetail(APIView):
+    def get(self, request, pk):
+        cat = get_object_or_404(Cat, id=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        cat = get_object_or_404(Cat, id=pk)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        cat = get_object_or_404(Cat, id=pk)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        cat = get_object_or_404(Cat, id=pk)
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
 @api_view(['GET', 'POST'])
@@ -31,3 +83,4 @@ def hello(request):
     # В ответ на GET-запрос нужно вернуть JSON
     # Он тоже будет создан из словаря, переданного в Response()
     return Response({'message': 'Это был GET-запрос!'})
+"""

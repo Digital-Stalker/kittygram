825878f152fe7b72469d195fc4214a21feb6b54d
Александр Ячменев <yachmenyov.alex@yandex.ru>
2023-03-30 15:14:11 +0700

    Lesson 6.

--- cats/views.py
+++ cats/views.py
@@ -9,7 +9,7 @@ from .serializers import CatSerializer
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -17,3 +17,17 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+# Применили декоратор и указали разрешённые методы
+def hello(request):
+    # По задумке, в ответ на POST-запрос нужно вернуть JSON с теми данными,
+    # которые получены в запросе.
+    # Для этого в объект Response() передаём словарь request.data.
+    if request.method == 'POST':
+        return Response({'message': 'Получены данные', 'data': request.data})
+
+    # В ответ на GET-запрос нужно вернуть JSON
+    # Он тоже будет создан из словаря, переданного в Response()
+    return Response({'message': 'Это был GET-запрос!'})
